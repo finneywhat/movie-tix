019e5454b6c09922f2b2777a0ae5fcfea2341265
Chris Finney and Riley Watts <chris@>
2017-02-28 13:52:58 -0800

    add javascript functionality to display total price, add major styling to styles.css, added form validation to correct NaN inputs

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,47 @@ Ticket.prototype.ticketPrice = function() {
 }
 
 
+
+
 $(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    var inputtedMovie = $("#movie").val();
+    var inputtedMovie = parseInt($("#movie").val());
     var inputtedTime = parseInt($("#time").val());
     var inputtedAge = parseInt($("#age").val());
-    var originalMovie= $("#movie").val();
-    console.log(inputtedMovie);
-    var ticket = new Ticket(inputtedMovie, inputtedTime, inputtedAge);
-    // var ticketticket.ticketPrice());
 
-  })
-})
+    var ticket = new Ticket(inputtedMovie, inputtedTime, inputtedAge);
+
+    var ticketOutput = ticket.ticketPrice();
+
+    if (!inputtedMovie || !inputtedTime || !inputtedAge) {
+      $(".alert").show();
+      $("#outputText").hide();
+    } else {
+      $(".alert").hide();
+      $("#outputText").show();
+      $("#priceOfTicket").text(ticketOutput);
+      $("#movie").val("");
+      $("#time").val("");
+      $("#age").val("");
+    };
+
+  });
+});
+
+
+// function ticketLogic () {
+//   if(ticket.movie === "La La Land") {
+//     total=total+2;
+//
+//   }
+//   else {
+//     if(ticket.time !== "11:00 a.m.") {
+//       if(ticket.age === "whatever") {
+//         total=
+//       } else {
+//         return
+//       }
+//     }
+//   }
+// }
